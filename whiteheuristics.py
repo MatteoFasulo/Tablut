b0da79d5b4857d826746e5aa2b022fe506311685
BandoleroNext <norbycasa@gmail.com>
2023-11-08 12:55:59 +0100

    King distance and movement heuristic added

--- whiteheuristics.py
+++ whiteheuristics.py
@@ -44,6 +44,40 @@ def white_fitness(board, alpha0, beta0, gamma0):
                             if board._is_there_a_clear_view(white, [i,white[1]]):
                                 fitness += alpha0
                                 break
+        
+        #King Distance: 
+        #Calculate the fastest path out for the king
+        #Calculate the number of black pieces in each quadrant
+        #The more black pieces there are in the quadrant, the lower the fitness
+        #TODO: fix the fitness number and the beta0 value
+        if board.king[0] > 4 and board.king[1] > 4:
+            #King is in the bottom right quadrant
+            bp = board.check_num_pieces_in_quadrant(1,1)
+            fitness += beta0 * 1-(bp*0.2)
+        if board.king[0] > 4 and board.king[1] < 4:
+            #King is in the bottom left quadrant
+            bp = board.check_num_pieces_in_quadrant(3,1)
+            fitness += beta0 * 1-(bp*0.2)
+        if board.king[0] < 4 and board.king[1] > 4:
+            #King is in the top right quadrant
+            bp = board.check_num_pieces_in_quadrant(2,1)
+            fitness += beta0 * 1-(bp*0.2)
+        if board.king[0] < 4 and board.king[1] < 4:
+            #King is in the top left quadrant
+            bp = board.check_num_pieces_in_quadrant(4,1)
+            fitness += beta0 * 1-(bp*0.2)
+
+        if board.king[0] != 4 and board.king[1] != 4:
+            if board._is_there_a_clear_view(board.king, [board.king[0],0]) or \
+            board._is_there_a_clear_view(board.king, [board.king[0],len(board.pieces)-1]) or\
+            board._is_there_a_clear_view(board.king, [0,board.king[1]]) or \
+            board._is_there_a_clear_view(board.king, [len(board.pieces)-1,board.king[1]]):
+                fitness += 1000 #Maximum value of heuristic
+
+
+        
+
+
 
         #"Pedoni esterna euristica più alta"
         #Since the fitness is computed on a static board, and not on a move, I have to count the number of pieces _far_ from the castle. The further they are as a whole, the higher the fitness
@@ -52,7 +86,7 @@ def white_fitness(board, alpha0, beta0, gamma0):
         #This heuristic HAS to be balanced with others, because it can lead to a situation where the white pieces are too far from the castle and the black pieces can easily win
         #The castle is at [4,4]
         for white in board.whites:
-            fitness += beta0 * ((white[0] - 4)**2 + (white[1] - 4)**2)**0.5
+            fitness += beta0 * ((white[0] - board.king[0])**2 + (white[1] - board.king[1])**2)**0.5
 
         #TODO: implement the special capture rule
         #King defence: If my king can be killed in the next move by the black player, apply a very strong negative fitness (static analysis)
